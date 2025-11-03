260ef68cacb8e6f90de948df45da6f527a2f4ebf
Lukasz <ukasz@MacBook-Pro-ukasz.local>
2025-11-03 18:14:44 +0000

    resnet backbone added

--- model.py
+++ model.py
@@ -1,4 +1,5 @@
 import torch.nn as nn
+import torchvision.models as models
 
 class VegetableNet(nn.Module):
   def __init__(self, num_classes=15, dropout_rate=0.4):
@@ -32,3 +33,25 @@ class VegetableNet(nn.Module):
     x = self.conv(x)
     x = self.dropout(x)
     return self.fc(x)
+
+
+class VegetableResNet(nn.Module):
+  def __init__(self, num_classes=15, dropout_rate=0.4, pretrained=True):
+    super().__init__()
+    self.resnet = models.resnet50(pretrained=pretrained)
+
+    self.resnet = nn.Sequential(*list(self.resnet.children())[:-1])
+
+    self.classifier = nn.Sequential(
+        nn.Flatten(),
+        nn.Dropout(dropout_rate),
+        nn.Linear(2048, 512),
+        nn.ReLU(),
+        nn.Dropout(dropout_rate),
+        nn.Linear(512, num_classes)
+    )
+
+  def forward(self, x):
+    x = self.resnet(x)
+    x = self.classifier(x)
+    return x
